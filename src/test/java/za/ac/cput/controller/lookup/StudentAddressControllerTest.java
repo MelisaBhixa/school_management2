5aa7e5b429d5e57f76671fb33d2ff906b6b55856
legiste99 <217046207@mycput.ac.za>
2022-06-18 20:18:45 +0200

    Empty Controller test classes

---
+++ src/test/java/za/ac/cput/controller/lookup/StudentAddressControllerTest.java
@@ -0,0 +1,7 @@
+package za.ac.cput.controller.lookup;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentAddressControllerTest {
+
+}
---
+++ src/test/java/za/ac/cput/controller/userInfo/NameControllerTest.java
@@ -0,0 +1,7 @@
+package za.ac.cput.controller.userInfo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NameControllerTest {
+
+}
