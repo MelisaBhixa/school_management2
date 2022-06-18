343b79d3b88e23e4b14b1e745360437c56020c5e
ZFukula <217299911@mycput.ac.za>
2022-06-18 22:30:31 +0200

    Student and Student Address Factory Tests Added Zuko Fukula 217299911

--- src/test/java/za/ac/cput/factory/lookup/StudentAddressFactoryTest.java
+++ src/test/java/za/ac/cput/factory/lookup/StudentAddressFactoryTest.java
@@ -1,7 +1,48 @@
+/**
+ StudentAddressFactoryTest.java
+ Factory test for StudentAddresss
+ Author: Zuko Fukula (217299911)
+ Date: 16 June 2022
+ */
+
 package za.ac.cput.factory.lookup;
 
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import za.ac.cput.domain.location.Address;
+import za.ac.cput.domain.lookup.StudentAddress;
 import static org.junit.jupiter.api.Assertions.*;
 
 class StudentAddressFactoryTest {
+
+    @Test
+    public void invalidTest(){
+        StudentAddress studentAddress = StudentAddressFactory.newStudentAddress("217299911", new Address());
+        assertNull(studentAddress.getStudentId());
+        System.out.println(studentAddress);
+    }
+
+    @Test
+
+    public void equalityValidTest(){
+        StudentAddress studentAddress = StudentAddressFactory.newStudentAddress("217299911", new Address());
+        System.out.println("StudentID: "+ studentAddress.getStudentId());
+        assertEquals("217299911", studentAddress.getStudentId());
+    }
+
+    @Test
+    public void validTest(){
+        StudentAddress studentAddress = StudentAddressFactory.newStudentAddress("", new Address());
+        assertNull(studentAddress.getStudentId());
+        System.out.println(studentAddress);
+    }
+
+    @Disabled("Test Disabled")
+    @Test
+
+    public void disableTest(){
+        StudentAddress studentAddress = StudentAddressFactory.newStudentAddress("217299911", new Address());
+        assertSame("217299911", studentAddress.getStudentId());
+    }
 
 }
--- src/test/java/za/ac/cput/factory/users/StudentFactoryTest.java
+++ src/test/java/za/ac/cput/factory/users/StudentFactoryTest.java
@@ -1,7 +1,50 @@
+/**
+ StudentFactoryTest.java
+ Factory test for Student
+ Author: Zuko Fukula (217299911)
+ Date: 16 June 2022
+ */
+
 package za.ac.cput.factory.users;
 
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import za.ac.cput.domain.userInfo.Name;
+import za.ac.cput.domain.users.Student;
+import za.ac.cput.factory.userInfo.NameFactory;
 import static org.junit.jupiter.api.Assertions.*;
 
 class StudentFactoryTest {
 
+    Name name = NameFactory.newName("Zuko", "Joe", " Fukula");
+
+    @Test
+    public void invalidTest(){
+        Student student = StudentFactory.newStudent("217299911", "217299911@mycput.ac.za", name);
+        assertNull(student.getStudentId());
+        System.out.println(student);
+    }
+
+    @Test
+
+    public void equalityValidTest(){
+        Student student = StudentFactory.newStudent("217299911", "217299911@mycput.ac.za", name);
+        System.out.println("Email: "+ student.getEmail());
+        assertEquals("217299911@mycput.ac.za", student.getEmail());
+    }
+
+    @Test
+    public void validTest(){
+        Student student = StudentFactory.newStudent("", "217299911@mycput.ac.za", name);
+        assertNull(student.getStudentId());
+        System.out.println(student);
+    }
+
+    @Disabled("Test Disabled")
+    @Test
+
+    public void disableTest(){
+        Student student = StudentFactory.newStudent("217299911", "217299911@mycput.ac.za", name);
+        assertSame("217299911", student.getStudentId());
+    }
 }
